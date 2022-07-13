73edff9009adeb0c3e720c25f9ea461fa5e723e0
parksangau <devsangau@gmail.com>
2022-07-13 11:55:44 +0900

    ImageViewMission

--- imageViewPractice/ViewController.swift
+++ imageViewPractice/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numImage = 1
+    var maxImage = 6
+    
+    @IBOutlet var imgView: UIImageView!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+           // Do any additional setup after loading the view.
+        imgView.image = UIImage(named: "1.png")
     }
-
-
+    //[이전] 버튼에 대한 액션 함수
+    @IBAction func btnPrevImage(_ sender: UIButton) {
+        numImage = numImage - 1
+        if (numImage < 1){
+            numImage = maxImage
+        }
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
+    //[다음] 버튼에 대한 액션 함수
+    @IBAction func btnNextImage(_ sender: UIButton) {
+        numImage = numImage + 1
+        if (numImage > maxImage) {
+            numImage = 1
+        }
+        let imageName = String(numImage) + ".png"
+        imgView.image = UIImage(named: imageName)
+    }
 }
-
